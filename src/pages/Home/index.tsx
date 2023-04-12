3c2bdc202a08dc8275761f09622265f07503e288
João Gabriel <jgomachado@hotmail.com>
2023-04-12 09:02:30 -0300

    initial commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,77 @@
+import { HandPalm, Play } from 'phosphor-react'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Countdown } from './components/Countdown'
+import { NewCycleForm } from './components/NewCycleForm'
+import { useContext } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+// Controlled (monitora o valor) / uncontrolled (não monitora o valor)
+// OnChange={(e) => setTask(e.target.value)} (monitorando cada letra que entra no input)
+// register()
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'o ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'o ciclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+  const { activeCycle, createNewCycle, interruptCurrentCycle } =
+    useContext(CyclesContext)
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCrateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisable = !task
+
+  // Prop Drilling -> Quando a gente tem MUITAS propriedades APENAS para comunicação entre componentes
+  // Context API -> Permite compartilharmos informações entre VÁrios componentes ao mesmo tempo
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCrateNewCycle)} action="">
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
+
+        {activeCycle ? (
+          <StopCountdownButton onClick={interruptCurrentCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisable} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
